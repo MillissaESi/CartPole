24bbf9fd7477aa03072656e49e742e9e923525be
System Administrator <root@wifi-10-30-29-227.wifi.univ-paris5.fr>
2019-12-27 14:28:24 +0100

    Monte Carlo

--- cart_pole.py
+++ cart_pole.py
@@ -1,11 +1,12 @@
 import numpy as np
 import gym
 
-nval = 6 # nombre de valeurs discrètes qu’une variable peut prendre
-N= nval ** 4  # Puisqu’il y a 4 variables, la taille de l’espace est nval4
+'''
+  The presentation of the different states 
+'''
 def discretise(x,mini,maxi):
     # discretise x
-    # renvoie un entier entre 0 et nval-1
+    # return an integer between 0 and nval - 1
     if x<mini: x=mini
     if x>maxi: x=maxi
     return int(np.floor((x-mini)*nval/(maxi-mini+0.0001)))
@@ -17,4 +18,25 @@ def observation_vers_etat(observation):
     pos2 = discretise(observation[3],mini=-1,maxi=1)
     return pos + vel*nval + angle*nval*nval + pos2*nval*nval*nval
 
+'''
+    Monte Carlo algorithm 
+'''
 
+
+
+
+
+
+
+nval = 6 # number of values for each observation variable
+N= nval ** 4  # total number of states
+env = gym.make('CartPole-v1')
+env.reset()
+nbIt=0
+done=False
+while not done:
+    observation, reward, done, info = env.step(np.random.randint(2))
+    env.render()
+    nbIt+=1
+print("Episode terminé après itérations".format(nbIt))
+env.close()
